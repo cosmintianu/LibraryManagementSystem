ec7f033fb1f77f5c4c6c536f5663d5665538f46a
cosmintianu <cosmin.tianunicolae@yahoo.com>
2025-05-11 19:16:58 +0100

    Add project files.

---
+++ LibraryManagementSystem/Controllers/BooksController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                Quantity = 1925
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "To Kill a Mockingbird",
+                Author = "Harper Lee",
+                Quantity = 1960
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "1984",
+                Author = "George Orwell",
+                Quantity = 1949
+            },
+            new Book
+            {
+                Id = 4,
+                Title = "Pride and Prejudice",
+                Author = "Jane Austen",
+                Quantity = 1813
+            },
+            new Book
+            {
+                Id = 5,
+                Title = "Moby-Dick",
+                Author = "Herman Melville",
+                Quantity = 1851
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book newBook)
+        {
+            if (newBook == null)
+            {  return BadRequest(); }
+
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetBookById), new {id = newBook.Id}, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, Book updatedBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound(updatedBook.Id);
+            }
+
+            book.Id = updatedBook.Id;
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Quantity = updatedBook.Quantity;
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id) 
+        {
+
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            books.Remove(book);
+
+            return NoContent(); 
+        }
+
+
+    }
+}
